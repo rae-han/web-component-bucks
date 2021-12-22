12e9ef6f693a816c7f549997493f7746399db0f5
Raehan <raehan900809@gmail.com>
2021-12-22 21:38:33 +0900

    add: menu isSoldOut attribute

--- client/src/js/components/MenuItemComponent.js
+++ client/src/js/components/MenuItemComponent.js
@@ -1,55 +1,31 @@
 export default class MenuItemComponent extends HTMLElement {
   constructor() {
     super();
+
+    this.toggle = this.toggle.bind(this);
+
+    this.render();
   }
   connectedCallback() {
-    console.log('before render');
-    this.render();
-    console.log('created');
+    this.toggleBtn.addEventListener('click', this.toggle);
   }
-  disconnectedCallback() {
-    console.log('disconnected');
-  }
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name, oldValue, newValue);
-  }
-  static get observedAttributes() {
-    return ['id'];
-  }
-  update() {
-    console.log('update');
-    this.render();
 
-  }
-  firstUpdated() {
-    console.log('firstUpdated');
-    this.render();
-
-  }
-  updated() {
-    console.log('updated');
-    this.render();
-
-  }
   render() {
-    console.log('render');
+    console.log(2, 'render');
     let $li = document.createElement('li');
     $li.classList = "menu-list-item d-flex items-center py-2";
-    $li.textContent = 'list item';
 
     let $span = document.createElement('span');
-    $span.classList = "w-100 pl-2 menu-name";
-    $span.textContent = name;
+    $span.classList = `w-100 pl-2 menu-name ${this.isSoldOut ? 'sold-out' : ''}`;
+    this.menuName = $span;
+    $span.textContent = this.name;
     $li.appendChild($span);
 
     let $toggleBtn = document.createElement('button');
     $toggleBtn.classList = "bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button";
     $toggleBtn.textContent = "품절";
     $li.appendChild($toggleBtn);
-    $toggleBtn.onclick = () => { 
-      console.log(this.id)
-      this.id = 123 
-    }
+    this.toggleBtn = $toggleBtn;
 
     let $updateBtn = document.createElement('button');
     $updateBtn.classList = "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
@@ -62,13 +38,49 @@ export default class MenuItemComponent extends HTMLElement {
     $li.appendChild($removeBtn);
 
     this.appendChild($li);
-
-    console.log(this.id)
   }
+
+  toggle() {
+    this.isSoldOut = !this.isSoldOut;
+  }
+
+  static get observedAttributes() { // 이 메서드를 통해 아래 attributeChangedCallback을 실행해준다.
+    console.log(0, 'get observedAttributes')
+    return ['id', 'name', 'issoldout']; // 여기 없는 값은 attributeChangedCallback 에서 감지 못한다.
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(4, `attributeChangedCallback oldValue: ${oldValue}, newValue: ${newValue}`);
+    this.menuName.classList = `w-100 pl-2 menu-name ${this.isSoldOut ? 'sold-out' : ''}`
+  }
+
+  // GET
   get id() {
     return this.getAttribute('id');
   }
+  get name() {
+    return this.getAttribute('name');
+  }
+  get isSoldOut() {
+    console.log(this.getAttribute('isSoldOut'))
+    return this.getAttribute('isSoldOut') === 'true';
+  }
+
+  // SET
   set id(newValue) {
     this.setAttribute('id', newValue);
   }
+  set name(newValue) {
+    this.setAttribute('name', newValue);
+  }
+  set isSoldOut(newValue) {
+    console.log('set', newValue)
+    this.setAttribute('isSoldOut', newValue);
+  }
+
+  // unmounted
+  disconnectedCallback() {
+    
+  }
 }
+
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 import MenuItemComponent from './components/MenuItemComponent.js';
 // import './components/SampleCounter.js';
-import './components/MyCounter.js';
-import './components/CustomButton.js';
+// import './components/MyCounter.js';
+// import './components/CustomButton.js';
 
 customElements.define('menu-item', MenuItemComponent);
